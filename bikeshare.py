e11e32ce92ba125863b2eef333122ca4c57e522a
Thomas Akkerhuis <thomas.akkerhuis@shell.com>
2020-03-11 19:58:23 +0100

    refactor: removed hello message
    code starts with a fake-friendly hello message. this commit removes it
    because it comes across insincere.

--- bikeshare.py
+++ bikeshare.py
@@ -15,7 +15,6 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
 
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs    
     cities = ['chicago', 'new york city', 'washington']
